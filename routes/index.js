1f8079bb24ebfb39215c133631b5f86a1a6a12f2
Michal Novemsky <michal.novemsky@gmail.com>
2015-11-13 17:56:02 -0500

    removed the tweetbank stuff from routes/index.js

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,18 @@
 var express = require('express');
 var router = express.Router();
 // could use one line instead: var router = require('express').Router();
-var tweetBank = require('../tweetBank');
+//var tweetBank = require('../tweetBank');
 
 router.get('/', function (req, res) {
-  var tweets = tweetBank.list();
-  res.render( 'index', { title: 'Twitter.js', tweets: tweets } );
+  //var tweets = tweetBank.list();
+  //res.render( 'index', { title: 'Twitter.js', tweets: tweets } );
+  //TODO: get all the tweets without tweetBank
 });
 
 function getTweet (req, res){
-  var tweets = tweetBank.find(req.params);
-  res.render('index', { tweets: tweets });
+  //var tweets = tweetBank.find(req.params);
+  //res.render('index', { tweets: tweets });
+  //TODO: get a tweet without tweetBank
 }
 
 router.get('/users/:name', getTweet);
@@ -20,7 +22,8 @@ router.get('/users/:name/tweets/:id', getTweet);
 router.post('/submit', function(req, res) {
   var name = req.body.name;
   var text = req.body.text;
-  tweetBank.add(name, text);
+  //tweetBank.add(name, text);
+  //TODO: add a tweet without tweetBank
   res.redirect('/');
 });
 
